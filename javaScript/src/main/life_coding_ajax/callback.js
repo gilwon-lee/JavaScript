40f0e2fd12397d5273fa224feb4a87ee2622038e
lee <bluesky930401@gmail.com>
2022-07-05 23:56:41 +0900

    220705 commit

--- javaScript/src/main/life_coding_ajax/callback.js
+++ javaScript/src/main/life_coding_ajax/callback.js
@@ -12,5 +12,27 @@ window.addEventListener("load",()=>{
     }
     newWords = myfilter(words,element=>element.length>6);
     console.log(newWords);
+    
+    //fetch 함수는 promise 타입을 리턴한다. ->비동기 함수일 확률이 높음
+    //  fetch("https://jsonplaceholder.typicode.com/posts").then(
+    //  function(response){
+    //     return response.json();
+    //  }).then(function(myJson){
+    //     console.log(JSON.stringify(myJson));
+    // });
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(function(response){
+            // response.json().then(function(data){
+            //     console.log('data:',data);
+            // });
+            return response.json();
+        })
+        .catch(function(reason){
+            console.log('reason:',reason);
+        }).then(function(data){
+            console.log('data',data);
+    })
 });
+
 

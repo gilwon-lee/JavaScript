b00621a838a9c4ed551af9d3d67c937e54442b37
lee <bluesky930401@gmail.com>
2022-07-06 23:55:52 +0900

    220706 commit

--- javaScript/src/main/life_coding_ajax/callback.js
+++ javaScript/src/main/life_coding_ajax/callback.js
@@ -21,18 +21,52 @@ window.addEventListener("load",()=>{
     //     console.log(JSON.stringify(myJson));
     // });
 
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(function(response){
-            // response.json().then(function(data){
-            //     console.log('data:',data);
-            // });
-            return response.json();
-        })
-        .catch(function(reason){
-            console.log('reason:',reason);
-        }).then(function(data){
-            console.log('data',data);
-    })
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //     .then(function(response){
+    //         // response.json().then(function(data){
+    //         //     console.log('data:',data);
+    //         // });
+    //         return response.json();
+    //     })
+    //     .catch(function(reason){
+    //         console.log('reason:',reason);
+    //     }).then(function(data){
+    //         console.log('data',data);
+    // })
+    function timer(time){
+        return new Promise(function(resolve){
+            setTimeout(function(){
+                resolve(time);
+            },time);
+        });
+    }
+    /*
+    console.log('start');
+    timer(1000).then(function(time){
+        console.log('time:',time);
+        return timer(time+1000);
+    }).then(function(time){
+        console.log('time:',time);
+        return timer(time+1000);
+    });
+    */
+   //async -> 비동기 함수 promise 리턴
+    async function run(){
+        console.log('start');
+        let time = await timer(1000);
+        console.log('time:'+time);
+        time = await timer(time+1000);
+        console.log('time:'+time);
+        time = await timer(time+1000);
+        console.log('time:'+time);
+        console.log('end');
+    }
+    async function run2(){
+        console.log('parent start');
+        await run();
+        console.log('parent end');
+    }
+    run2();
 });
 
 

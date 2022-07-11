10fa0f8a902e1efa44482ba3bb58f2aded062404
lee <bluesky930401@gmail.com>
2022-07-11 23:18:17 +0900

    220711 commit

--- javaScript/src/main/devil_js/closure.js
+++ javaScript/src/main/devil_js/closure.js
@@ -11,3 +11,32 @@ function makeAdder(x){
 
 const add3 = makeAdder(3);
 console.log(add3(2)); //5
+
+
+let lo = '10';
+function fn1(){
+    let l1 ='l1';
+    console.log(lo,l1);
+    fn2();
+}
+function fn2(){
+    let l2 ='l2';
+    // console.log(lo,l1,l2); <- 접근 불가
+}
+
+fn1();
+
+function fac(num1){
+    function plus(num2){
+        return num1+num2;
+    }
+    return plus;
+}
+
+let plus1 = fac(1);
+console.log(plus1(1));
+console.log(plus1(2));
+
+let plus2 = fac(2);
+console.log(plus2(1));
+console.log(plus2(2));
